eda982f1b210001b33368f18292469cc055700ad
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-01 15:42:36 +0300

    Create | Create a Post function - routes/posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,22 @@
 const router = require("express").Router();
+const Post = require("../models/Post");
 
-router.get("/", (req,res)=>{
-    console.log("POST PAGE");
+// Create a post 
+router.post("/", async(req,res)=> {
+    const newPost = new Post(req.body)
+    try {
+        // I'm gonna create new one, so
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
+
+// Update a post 
+// Delete a post 
+// Like a post 
+// Get a post 
+// Get timeline post // all following's posts
 
 module.exports = router;
