25138d8c61add774d8e6f9aa7ae57e7e6660b674
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-01 09:56:12 +0300

    Create POST method for /login | Login and Register System

--- routes/auth.js
+++ routes/auth.js
@@ -21,8 +21,25 @@ router.post("/register", async (req, res) => {
         const user = await newUser.save();
         res.status(200).json(user)
     } catch (error) {
-        console.log(error);
+        res.status(500).json(err)
     }
 })
+
+// LOGIN
+router.post("/login", async (req,res) => {
+    try {
+        const user = await User.findOne({email:req.body.email});
+        !user && res.status(404).json("user not found.")
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        !validPassword && res.status(400).json("wrong password"); // 400 - wrong password
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json(err)
+    }
+}) 
+
+
 
 module.exports = router
