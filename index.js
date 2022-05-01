da4cf38206751f095b1bc027aa45416f7a6fb99f
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-01 06:46:21 +0300

    Middlewares and First Request

--- index.js
+++ index.js
@@ -14,6 +14,19 @@ mongoose.connect(
         console.log("Connected to MONGODB");
     })
 
+//middleware
+app.use(express.json()); // body parser when you make  post request.. İt just gonna password it
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req,res)=>{
+    res.send("Welcome to homepage")
+})
+
+app.get("/users", (req,res)=>{
+    res.send("Welcome to user page")
+})
+
 app.listen(8800, () => {
     console.log("Backend server is running!");
 })
