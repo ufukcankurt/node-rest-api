a9fe3bff178d5062f27afc6d9f1cdf0c6e106b20
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-01 15:19:59 +0300

    Create | Post Model

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ app.use(morgan("common")); // show on terminal| your date and request type and a
 // address for rest API  || whenever go to this adress  it's gonna run this router.
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is running!");
---
+++ routes/posts.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+router.get("/", (req,res)=>{
+    console.log("POST PAGE");
+})
+
+module.exports = router;
