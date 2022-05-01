daf611918a4673652ee3b67cad762c75847cfc7d
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-01 16:40:09 +0300

    Create | Like / Dislike a Post function - routes/posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -50,7 +50,21 @@ router.delete("/:id", async (req,res)=>{
 })
 
 
-// Like a post 
+// Like / dislike a post 
+router.put("/:id/like", async (req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post.likes.includes(req.body.userId)){
+            await post.updateOne({$push:{likes:req.body.userId}});
+            res.status(200).json("The post has been liked.")
+        }else{
+            await post.updateOne({$pull:{likes:req.body.userId}});
+            res.status(200).json("The post has been DISLIKED.")
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
 // Get a post 
 // Get timeline post // all following's posts
 
