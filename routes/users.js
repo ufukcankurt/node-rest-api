f2664caa3af97a151a184af8adf447c389d8c02b
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-01 12:43:34 +0300

    Create | Get a User function - routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -44,7 +44,19 @@ router.delete("/:id", async (req, res) => {
         return res.status(403).json("You can delete only your account!")
     }
 })
+
+
 // Get a  User
+router.get("/:id", async (req,res)=>{
+    try {
+        const user = await User.findById(req.params.id)
+        const {password,updatedAt, createdAt, ...other} = user._doc;
+        res.status(200).json(other); // 200 --> successfull
+    } catch (error) {
+       res.status(500).json(error); 
+    }
+})
+
 // Follow a User
 // Unfollow a User
 
