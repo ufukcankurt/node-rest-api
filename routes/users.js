7b212456bd2eb0a52f4f7692eeeed0dc054737ad
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-01 14:00:17 +0300

    Create

--- routes/users.js
+++ routes/users.js
@@ -58,6 +58,26 @@ router.get("/:id", async (req,res)=>{
 })
 
 // Follow a User
+router.put("/:id/follow", async (req,res)=> {
+    if(req.body.userId !== req.params.id){ // same user
+        try {
+            const user = await User.findById(req.params.id) 
+            const currentUser = await User.findById(req.body.userId)
+            if(!user.followers.includes(req.body.userId)){
+                await user.updateOne({$push:{followers:req.body.userId}})
+                await currentUser.updateOne({$push:{followings:req.params.id}})
+                res.send(200).json("User has been followed")
+            }else{
+                res.status(403).json("You already follow this user.")
+            }
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }else{
+        res.status(403).json("You can't follow yourself.")
+    }
+})
+
 // Unfollow a User
 
 
