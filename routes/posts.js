63f226e5b6ee9e727ad7fd1828afc7ac9d41100c
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-01 16:21:05 +0300

    Create | Delete a Post function - routes/posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -32,7 +32,24 @@ router.put("/:id", async (req,res)=>{
         res.status(500).json(error)
     }
 })
+
+
 // Delete a post 
+router.delete("/:id", async (req,res)=>{
+    try {
+    const post = await Post.findById(req.params.id)
+    if(post.userId === req.body.userId){
+        await post.deleteOne();
+        res.status(200).json("The post has been deleted.")
+    }else{
+        res.status(403).json("You can delete only your post");
+    }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+
 // Like a post 
 // Get a post 
 // Get timeline post // all following's posts
